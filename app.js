729c58d6ed0826987b3838f724a0e704c893609a
chinosama <85371007+chinosama@users.noreply.github.com>
2021-06-22 14:51:18 -0300

    Funcionando

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3030;
 const path = require("path");
 app.use(express.static('public'));
 
-app.get("/baggage",(req,res) => res.sendFile(path.join(__dirname,"views","baggage.html")));
+app.get("/babbage",(req,res) => res.sendFile(path.join(__dirname,"views","babbage.html")));
 
 app.get("/berners-lee",(req,res) => res.sendFile(path.join(__dirname,"views","berners-lee.html")));
 
